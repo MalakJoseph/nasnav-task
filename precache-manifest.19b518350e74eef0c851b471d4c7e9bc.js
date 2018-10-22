1600cc1fbc6232b788db063402353f533ec4020a
Malak Joseph <malak_joseph2010@yahoo.com>
2018-10-22 19:30:05 +0200

    Updates

---
+++ precache-manifest.19b518350e74eef0c851b471d4c7e9bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed9940ba87fb54ac7c4a",
+    "url": "/nasnav-task/static/js/runtime~main.ed9940ba.js"
+  },
+  {
+    "revision": "e16833b0bde999e19646",
+    "url": "/nasnav-task/static/js/main.e16833b0.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/nasnav-task/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "e16833b0bde999e19646",
+    "url": "/nasnav-task/static/css/main.e5708c33.chunk.css"
+  },
+  {
+    "revision": "2c4bfbc930b1fcdc9e1c1992d834552f",
+    "url": "/nasnav-task/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/nasnav-task/precache-manifest.19b518350e74eef0c851b471d4c7e9bc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/nasnav-task/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
